f6ef81be3f887508af0782c7c074049ab65b82f7
adiq535 <adimamasaliev1552@gmail.com>
2020-05-12 02:49:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Runner runner1 = new Runner("Runner 1");
+        runner1.start();
+
+    }
+}
